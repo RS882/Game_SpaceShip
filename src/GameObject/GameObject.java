c57d2d4c38579573aed9b8e479731300e7aa47b7
Ruslan Senkin <rsenkin24@gmail.com>
2023-12-25 09:29:02 +0200

    add abstract class GameObject

---
+++ src/GameObject/GameObject.java
@@ -0,0 +1,25 @@
+package GameObject;
+
+abstract public class GameObject {
+    private double remainingStrength;
+
+    public GameObject(double remainingStrength) {
+        this.remainingStrength = remainingStrength;
+    }
+
+    public double getRemainingStrength() {
+        return this.remainingStrength;
+    }
+
+    public void setRemainingStrength(double remainingStrength) {
+        this.remainingStrength = remainingStrength;
+    }
+
+    abstract void destroy();
+
+    abstract void hurt(double amount);
+
+    public boolean isAlive() {
+        return this.remainingStrength > 0;
+    }
+}
